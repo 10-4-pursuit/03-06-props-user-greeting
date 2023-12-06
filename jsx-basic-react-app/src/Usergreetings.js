a8b4e550fdb8a2da4781d80d957d75f426c905d1
dttrue <danieltorres@pursuit.org>
2023-12-06 14:42:22 -0500

    Daniels props user greeting

---
+++ jsx-basic-react-app/src/Usergreetings.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Usergreetings = (props) => {
+
+    return (
+        <div>
+            <h1>Hello! {props.username}</h1>
+        </div>
+    );
+
+}
+
+export default Usergreetings
