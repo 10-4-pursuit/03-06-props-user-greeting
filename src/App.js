73c3cb114628daabd5f6c61702dc997eaf85a40f
EdgarCollado <edgarcollado@pursuit.org>
2023-12-09 16:53:51 -0500

    added-userGreeting-component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UserGreeting from './UserGreeting';
+
+function App() {
+  return (
+    <div className="App">
+      <UserGreeting username='Edgar C.'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UserGreeting.js
@@ -0,0 +1,7 @@
+
+
+function UserGreeting(props) {
+    return <h1>Hello, {props.username} wasn't this much easier? </h1>
+}
+
+export default UserGreeting;
