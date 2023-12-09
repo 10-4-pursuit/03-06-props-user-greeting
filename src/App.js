8690e4fd428170507e6111db26e615e1f4fc458b
athomas227 <andrewthomas@pursuit.org>
2023-12-09 16:03:01 -0500

    finished exercise

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import UserGreeting from './UserGreeting';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>test</h1>
+      <UserGreeting username="Sammy5150"/>
+      <UserGreeting username="Will3684"/>
+      <UserGreeting username="Jimmy0823"/>
+    </div>
+  );
+}
+
+export default App;
