dd3d5a35027476416113240558e58daf920b3f7a
AmiraFredericks <amirafredericks@pursuit.org>
2023-12-09 14:16:27 -0500

    Prop Greetings

---
+++ my-react-app/src/App.js
@@ -0,0 +1,16 @@
+import UserGreeting from './UserGreeting';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UserGreeting username = "Wonder Woman"/>
+      <UserGreeting username = "Batman"/>
+
+    
+      
+    </div>
+  );
+}
+
+export default App;
