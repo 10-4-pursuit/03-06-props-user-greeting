12ce13d396d267eaaeeb57c5a7162dc8dc6529ed
lanell1 <lanelllopez@pursuit.org>
2023-12-20 08:35:03 -0500

    03-06-props-user-greeting

---
+++ 03-06-props-user-greeting/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import UserGreeting from './UserGreeting';
+
+function App() {
+  return (
+    <div className="App">
+      <UserGreeting username="Lanell" />
+    </div>
+  );
+}
+
+export default App;
---
+++ 03-06-props-user-greeting/src/UserGreeting.js
@@ -0,0 +1,8 @@
+// UserGreeting.js
+import React from 'react';
+
+const UserGreeting = ({ username }) => {
+  return <div>Hello, {username}!</div>;
+};
+
+export default UserGreeting;
